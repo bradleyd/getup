793831ddb00a87bc6fc00731b4e73a50fc4b5f05
Brad Smith <bradleydsmith@gmail.com>
2013-05-15 16:38:02 -0400

    add better documentation

--- lib/getup.rb
+++ lib/getup.rb
@@ -3,8 +3,22 @@ require "getup/process"
 require "getup/notify"
 require "getup/timer_engine"
 
+# getup toplevel
 module Getup
+  # init is the public facing api to getup
   class Init
+    # @param options [Hash]
+    # @param name [String] name of the alert
+    # @param interval [Integer] amount of time in seconds for the alert
+    # @param message [String] message the alert will display
+    # @param repeat [Boolean] repeat the timer or not
+    #
+    # @example
+    #  Getup::Init.new(name: "foo",
+    #                  interval: 10, #seconds
+    #                  message: "this is the top level message",
+    #                  repeat: false) #this is a one off timer alert
+
     def initialize(options={})
       @name       = options.fetch(:name) { "foo" }
       @interval   = options.fetch(:interval) { 1.0 }
@@ -12,6 +26,8 @@ module Getup
       @repeat     = options.fetch(:repeat) { false } 
     end
 
+    # create timer object
+    # fire and forget process
     def run
       timer = TimerEngine.new(name: @name, 
                               interval: @interval, 
--- lib/getup/notify.rb
+++ lib/getup/notify.rb
@@ -4,21 +4,22 @@ module Getup
   # notfiy engine
   # @note currently this is used for Ubuntu only
   class Notify
-    def initialize(opts={})
-      @message = opts.fetch(:message) {"nothing important to say"}
-      @body    = opts.fetch(:body) { "woot!" }
-      @timeout = 0.5 # default
-      @engine  = Libnotify #.new(summary: @message,
-                 #              body: @body, 
-                 #              timeout: @timeout)
+    attr_accessor :notify_engine
+    # @param message [String] message to display
+    # @param body [String] secondary message to display
+    # @param notify_engine [Object] notification library
+    def initialize(options={}, notify_engine=Libnotify)
+      @message = options.fetch(:message) {"nothing important to say"}
+      @body    = options.fetch(:body) { "woot!" }
+      @timeout = options.fetch(:timeout) { 0.5}  
+      self.notify_engine  = notify_engine
     end
 
     # display the message
     def show
-      @engine.show(summary: @message,
-                   body: @body, 
-                   timeout: @timeout)
-
+      notify_engine.show(summary: @message,
+                         body: @body, 
+                         timeout: @timeout)
     end
   end
 end
